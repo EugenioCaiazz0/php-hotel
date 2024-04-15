fb4a3149321dab2908c68655a8f1e232e5abcfee
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-04-15 17:26:34 +0200

    Aggiunta selezione voto

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $hotels = [
         [
             'nome' => 'Habbo Hotel',
-            'descrizione' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto esse voluptas, quibusdam ipsam pariatur laboriosam! Minus nemo esse ex saepe repudiandae quia, magni quo expedita?',
+            'descrizione' => 'Habbo is one of the most popular wirtual worlds of the planet! Meet new people and start new friendships.',
             'parcheggio' => 'si',
             'img' => 'assets/habbo.jpeg',
             'voto' => 4,
@@ -10,15 +10,15 @@
         ],
         [
             'nome' => 'Heavenhold',
-            'descrizione' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto esse voluptas, quibusdam ipsam pariatur laboriosam! Minus nemo esse ex saepe repudiandae quia, magni quo expedita?',
+            'descrizione' => 'A link to the past! Relive your greatest adventures with Guardian Tales! From solving challenging puzzles in epic dungeons to strategic action combat, embark on this legendary journey to save the world of Kanterbury!',
             'parcheggio' => 'si',
             'img' => 'assets/Heavenhold.webp',
             'voto' => 2,
             'DDC' => 2
         ],
         [
-            'nome' => 'Jabba Palace',
-            'descrizione' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto esse voluptas, quibusdam ipsam pariatur laboriosam! Minus nemo esse ex saepe repudiandae quia, magni quo expedita?',
+            'nome' => 'Jabba`s Palace',
+            'descrizione' => 'Hutt Castle, often simply called Jabba the Hutt`s Palace after its owner, is a large sandrock and durasteel complex located at the fringes of the Northern Dune Sea on the planet of Tatooine.',
             'parcheggio' => 'no',
             'img' => 'assets/Jabbas_Palace.webp',
             'voto' => 1,
@@ -26,7 +26,7 @@
         ],
         [
             'nome' => 'Vault Tec Fallout Shelter',
-            'descrizione' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto esse voluptas, quibusdam ipsam pariatur laboriosam! Minus nemo esse ex saepe repudiandae quia, magni quo expedita?',
+            'descrizione' => 'Fallout Shelter puts you in control of a state-of-the-art underground Vault from Vault-Tec. Build the perfect Vault, keep your Dwellers happy, and protect them from the dangers of the Wasteland.',
             'parcheggio' => 'no',
             'img' => 'assets/Vault.jpeg',
             'voto' => 5,
@@ -34,7 +34,7 @@
         ],
         [
             'nome' => 'Monopoly Park',
-            'descrizione' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto esse voluptas, quibusdam ipsam pariatur laboriosam! Minus nemo esse ex saepe repudiandae quia, magni quo expedita?',
+            'descrizione' => 'I know you know.',
             'parcheggio' => 'si',
             'img' => 'assets/monopoly.jpeg',
             'voto' => 2,
@@ -42,9 +42,9 @@
         ],
     ];
 
-    /*$hotels_lenght = count($hotels);
+    /*$voto_ricerca = $_GET["voto"];
+    $hotels_lenght = count($hotels);
     var_dump($hotels_lenght); */
-
 ?>
 
 
@@ -54,11 +54,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title> PHP-Hotel</title>
 </head>
 
 <body>
+
+<form action="index.php" method="GET">
+<select class="form-select border-primary" aria-label="Rating-select" name="voto">
+  <option selected>Voto minimo</option> <!-- HO PROVATO A INCLUDERE LE ICON DI FONTAWESOME DIRETTAMENTE NELLE OPTIONS MA NON COMPAIONO, SERVE QUALCHE LINK ULTERIORE -->
+  <option value="1"> <span style="color: yellow;">&#9733</span> <span style="color: gray;"></span><span style="color: gray;"></span><span style="color: gray;"></span><span style="color: gray;"></span>
+  <option value="2"> <span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: gray;"></span><span style="color: gray;"></span><span style="color: gray;"></span>
+  <option value="3"><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: gray;"></span><span style="color: gray;"></span>
+  <option value="4"><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: gray;"></span>
+  <option value="5"><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span><span style="color: yellow;">&#9733</span>
+</select> 
+</form>
 
 <ul>
 <?php foreach($hotels as $hotel): ?>
